09b844076849740f703aa1756ede32255aa1f88c
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2022-09-21 15:05:41 +0300

    Names cleared, removed unused imports

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -3,9 +3,8 @@ from datetime import datetime
 import threading as th
 from argparse import ArgumentParser
 import websocket
-import json
-import base64
 import requests
+import os
 
 req_lock = th.Lock()
 got_req = False
@@ -22,7 +21,7 @@ wsapp.run_forever()
 
 def send(date, count, img):
     url = "http://"+server+":"+port+"/img"
-    r = requests.post(url, data={'date':date, 'count':count}, files={'img': img})
+    r = requests.post(url, data={'created_at':date, 'people_number':count}, files={'img': img})
 
 
 def wait_input():
@@ -34,7 +33,7 @@ def detect_faces():
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
     cap = cv2.VideoCapture(0)
-    prev_count = 0
+    prev_amount = 0
 
     frame_count = 0
     fps = int(cap.get(cv2.CAP_PROP_FPS))
@@ -50,18 +49,19 @@ def detect_faces():
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-            count = len(faces)
+            people_amount = len(faces)
 
             #color in BGR format
             with req_lock:
-                if got_req or prev_count != count:
+                if got_req or prev_amount != people_amount:
                     for (x, y, w, h) in faces:
                         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 1)
-                    prev_count = count
+                    prev_amount = people_amount
                     dt_str = dt.strftime("%d-%m-%Y_%H:%M:%S")
                     name = "./"+dt_str+".jpg"
                     _ = cv2.imwrite(name, img)
-                    send(dt_str, count, open(name, 'rb'))
+                    send(dt_str, people_amount, open(name, 'rb'))
+                    os.remove(name)
                     got_req = False
         frame_count += 1
 
