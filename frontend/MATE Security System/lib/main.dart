c47759ffc294fdac5fe24ba7133068ed6e603d8e
Azarolol <gimazovazamat@mail.ru>
2022-10-13 16:20:06 +0300

    added history

--- frontend/MATE Security System/lib/main.dart
+++ frontend/MATE Security System/lib/main.dart
@@ -9,6 +9,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+class ImageWithInfo {
+  late Image image;
+  late DateTime time;
+  late String peopleAmount;
+
+  ImageWithInfo(Image i, DateTime t, String pa) {
+    image = i;
+    time = t;
+    peopleAmount = pa;
+  }
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -72,76 +84,118 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late DateTime initialTime;
-  late final Timer timer;
   Duration elapsed = Duration.zero;
   String elapsedString = "0";
   String status = "ER";
   Color? statusColor = Colors.red[200];
-  Image image = Image.asset('assets/mate_logo.png', width: 360, height: 240);
   final ConnectivityResult connectionStatus = ConnectivityResult.none;
   final Connectivity connectivity = Connectivity();
   late StreamSubscription<ConnectivityResult> connectivitySubscription;
   ConnectivityResult connection = ConnectivityResult.none;
-  String peopleOnPhoto = "0";
   double width = 370;
-  double height = 210;
+  double height = 240;
+  double next = 0;
+  double previous = 0;
+  double buttonOn = 1;
+  double buttonOff = 0;
+  var images = [ImageWithInfo(Image.asset('assets/mate_logo.png', width: 370, height: 240), DateTime.now(), "0")];
+  int current = 0;
+  int max = 0;
+  String text = "minutes ago\n   updated";
 
   @override
   void initState() {
-    initialTime = DateTime.now();
     _refreshPicture();
     super.initState();
     connectivitySubscription =
         connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
           connection = result;
         });
-    timer = Timer.periodic(const Duration(minutes: 1), (_)
+    Timer.periodic(const Duration(minutes: 1), (_)
     {
-      final now = DateTime.now();
       setState(() {
-        elapsed = now.difference(initialTime);
-        if (elapsed.inMinutes > 60) {
-          elapsedString = ">60";
-        } else {
-          elapsedString = elapsed.inMinutes.toString();
-        }
+        updateTime();
       });
     });
   }
 
-  Future<String> _getPicture() async {
+  Future<void> _getPicture() async {
     final response = await http.get(Uri.parse('http://82.146.33.179:8000/api/v1/image'));
     if (response.statusCode == 200) {
       final octStream = response.body.substring(response.body.indexOf('application/octet-stream') + 28);
       final imageStream = octStream.substring(0, octStream.indexOf(RegExp(r'--[0-9a-z]*--')));
-      image = Image.memory(Uint8List.fromList(imageStream.codeUnits), width: 360, height: 240);
       final peopleNumber = response.body.substring(response.body.indexOf('"people_number":') + 16);
-      peopleOnPhoto = peopleNumber.substring(0, peopleNumber.indexOf('}'));
       final createdAt = response.body.substring(response.body.indexOf('"created_at":') + 14);
-      initialTime = DateTime.parse(createdAt.substring(0, createdAt.indexOf(',') - 1));
-      return "OK";
+      final initialTime = DateTime.parse(createdAt.substring(0, createdAt.indexOf(',') - 1));
+      if (images[current].time != initialTime) {
+        ImageWithInfo newPhoto = ImageWithInfo(
+            Image.memory(
+                Uint8List.fromList(imageStream.codeUnits),
+                width: width,
+                height: height
+            ),
+            initialTime,
+            peopleNumber.substring(0, peopleNumber.indexOf('}')));
+        images.add(newPhoto);
+        max++;
+        current = max;
+        if (current > 1) {
+          previous = buttonOn;
+        }
+        next = buttonOff;
+        updateTime();
+      }
+      status = "OK";
+      statusColor = Colors.green[200];
     } else {
-      throw Exception('Failed to load');
+      status = "ER";
+      statusColor = Colors.red[200];
     }
   }
 
   void _refreshPicture() {
     setState(() {
       _getPicture();
-        if (connection != ConnectivityResult.none) {
-          status = "OK";
-          statusColor = Colors.green[200];
-          elapsed = DateTime.now().difference(initialTime);
-          if (elapsed.inMinutes > 60) {
-            elapsedString = ">60";
-          } else {
-            elapsedString = elapsed.inMinutes.toString();
-          }
-        } else {
-          status = "ER";
-          statusColor = Colors.red[200];
-        }
+    });
+  }
+
+  void updateTime() {
+    elapsed = DateTime.now().difference(images[current].time);
+    if (elapsed.inHours > 24) {
+      text = " days ago\n updated";
+      elapsedString = elapsed.inDays.toString();
+    } else if (elapsed.inMinutes > 60) {
+      text = "hours ago\n  updated";
+      elapsedString = elapsed.inHours.toString();
+    } else {
+      text = "minutes ago\n   updated";
+      elapsedString = elapsed.inMinutes.toString();
+    }
+  }
+
+  void increaseCurrent() {
+    setState(() {
+      if (current < max) {
+        current++;
+        updateTime();
+        previous = buttonOn;
+      }
+      if (current == max) {
+        next = buttonOff;
+      }
+    });
+  }
+
+  void decreaseCurrent() {
+    setState(() {
+      if (current > 1) {
+        current--;
+        updateTime();
+        next = buttonOn;
+      }
+      if (current == 1) {
+        previous = buttonOff;
+      }
     });
   }
 
@@ -157,21 +211,24 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children:
             <Widget>[
-              Container(
-                          padding: const EdgeInsets.all(2),
-                          decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(20)),
-                          child: Stack(
-                              children: <Widget>[
-                                SizedBox(width: width, height: height, child: const Center(child:CircularProgressIndicator(strokeWidth: 4))),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child : Center(child: image)
-                          )])),
+                Container(
+                    padding: const EdgeInsets.all(2),
+                    decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(20)),
+                    child: Stack(
+                        children: <Widget>[
+                          SizedBox(width: width, height: height, child: const Center(child:CircularProgressIndicator(strokeWidth: 4))),
+                          SizedBox(
+                              width: width,
+                              height: height,
+                              child: images[current].image)])),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                          Column(children:<Widget>[Stack(
-                            children: <Widget>[Container(
+                    Column(
+                        children:<Widget>[
+                          Stack(
+                            children: <Widget>[
+                              Container(
                                 padding: const EdgeInsets.all(3),
                                 decoration: const BoxDecoration(color: Colors.black, shape: BoxShape.circle),
                                 child: Container(
@@ -181,66 +238,141 @@ class _MyHomePageState extends State<MyHomePage> {
                                     color: Colors.brown[200],
                                     shape: BoxShape.circle,
                                   ),
-                                  child: Center(widthFactor: 2.7, heightFactor: 1.4, child: Text(elapsedString, style: const TextStyle(fontSize: 40))),
+                                  child: Center(widthFactor: 2.7, heightFactor: 1.4, child: Text(elapsedString, style: const TextStyle(fontSize: 40)))
                                 )
-                            ),
+                              )
                             ]
-                            ),
-                          const Text("minutes ago\n   updated", style: TextStyle(fontSize: 15),)]),
-                          Column(children:<Widget>[Stack(
-                            children: <Widget>[Container(
-                            padding: const EdgeInsets.all(3),
-                            decoration: const BoxDecoration(color: Colors.black, shape: BoxShape.circle),
-                            child: Container(
-                              width: 80.0,
-                              height: 80.0,
-                              decoration: BoxDecoration(
-                                color: Colors.brown[200],
-                                shape: BoxShape.circle,
-                              ),
-                              child: Center(widthFactor: 2.7, heightFactor: 1.4, child: Text(peopleOnPhoto, style: const TextStyle(fontSize: 40))),
-                            )
-                        ),
-                            ]),
-                            const Text("people on\n    photo", style: TextStyle(fontSize: 15),)]),
-                          Column(children:<Widget>[Stack(
-                            children: <Widget>[Container(
-                            padding: const EdgeInsets.all(3),
-                            decoration: const BoxDecoration(color: Colors.black, shape: BoxShape.circle),
-                            child: Container(
-                              width: 80.0,
-                              height: 80.0,
-                              decoration: BoxDecoration(
-                                color: statusColor,
-                                shape: BoxShape.circle,
-                              ),
-                              child: Center(widthFactor: 1.5, heightFactor: 1.8, child: Text(status, style: const TextStyle(fontSize: 40)))),
-                            )]
-                        ),
-
-                            const Text("connection\n    status", style: TextStyle(fontSize: 15),)])
-                          ]
+                          ),
+                          Text(text, style: const TextStyle(fontSize: 15))
+                        ]
+                    ),
+                    Column(
+                        children:<Widget>[
+                          Stack(
+                              children: <Widget>[
+                                Container(
+                                    padding: const EdgeInsets.all(3),
+                                    decoration: const BoxDecoration(color: Colors.black, shape: BoxShape.circle),
+                                    child: Container(
+                                        width: 80.0,
+                                        height: 80.0,
+                                        decoration: BoxDecoration(
+                                          color: Colors.brown[200],
+                                          shape: BoxShape.circle,
+                                        ),
+                                        child: Center(widthFactor: 2.7, heightFactor: 1.4, child: Text(images[current].peopleAmount, style: const TextStyle(fontSize: 40)))
+                                    )
+                                )
+                              ]
+                          ),
+                          const Text("people on\n    photo", style: TextStyle(fontSize: 15))
+                        ]
+                    ),
+                    Column(
+                        children:<Widget>[
+                          Stack(
+                              children: <Widget>[
+                                Container(
+                                    padding: const EdgeInsets.all(3),
+                                    decoration: const BoxDecoration(color: Colors.black, shape: BoxShape.circle),
+                                    child: Container(
+                                        width: 80.0,
+                                        height: 80.0,
+                                        decoration: BoxDecoration(
+                                          color: statusColor,
+                                          shape: BoxShape.circle,
+                                        ),
+                                        child: Center(widthFactor: 1.5, heightFactor: 1.8, child: Text(status, style: const TextStyle(fontSize: 40))))
+                                )
+                              ]
+                          ),
+                          const Text("connection\n    status", style: TextStyle(fontSize: 15))
+                        ]
+                    )
+                  ]
                 ),
-                Container(
-                  padding: const EdgeInsets.all(2),
-                  decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(20)),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: TextButton(
-                              style: TextButton.styleFrom(
+                Column(
+                  children: [
+                    Container(
+                        padding: const EdgeInsets.all(2),
+                        decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(20)),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: TextButton(
+                                style: TextButton.styleFrom(
                                   fixedSize: const Size(320, 60),
                                   foregroundColor: Colors.black,
                                   backgroundColor: Colors.brown[200],
-                              ),
-                              onPressed: _refreshPicture,
-                              child: const Text(
+                                ),
+                                onPressed: _refreshPicture,
+                                child: const Text(
                                   "Update Now",
-                              style: TextStyle(
-                                fontSize: 35
-                              ),
+                                  style: TextStyle(
+                                      fontSize: 35
+                                  ),
+                                )
+                            )
+                        )
+                    ),
+                    Center(
+                      child:
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Opacity(
+                                opacity: previous,
+                                child:
+                                  Container(
+                                      padding: const EdgeInsets.all(2),
+                                      decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(20)),
+                                      child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(20),
+                                          child: TextButton(
+                                              style: TextButton.styleFrom(
+                                                fixedSize: const Size(160, 60),
+                                                foregroundColor: Colors.black,
+                                                backgroundColor: Colors.brown[200],
+                                              ),
+                                              onPressed: decreaseCurrent,
+                                              child: const Text(
+                                                "Previous",
+                                                style: TextStyle(
+                                                    fontSize: 35
+                                                ),
+                                              )
+                                          )
+                                      )
+                                  )
+                            ),
+                          Opacity(
+                            opacity: next,
+                            child:
+                              Container(
+                                  padding: const EdgeInsets.all(2),
+                                  decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(20)),
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: TextButton(
+                                          style: TextButton.styleFrom(
+                                            fixedSize: const Size(160, 60),
+                                            foregroundColor: Colors.black,
+                                            backgroundColor: Colors.brown[200],
+                                          ),
+                                          onPressed: increaseCurrent,
+                                          child: const Text(
+                                            "Next",
+                                            style: TextStyle(
+                                                fontSize: 35
+                                            ),
+                                          )
+                                      )
+                                  )
                               )
-                          )
-                      )
+                            )
+                          ],
+                        )
+                    )
+                  ],
                 )
             ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
